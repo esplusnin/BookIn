9e5f3b1d15abbe52a5185040096a638f5401a8a6
esplusnin <esplusnin94@yandex.ru>
2023-09-26 12:36:13 +0400

    Засетить первичную настройку проекта
    Добавил кодогенерацию строк, свитфлинт, шрифты и цвета по макету

---
+++ BookIn/SupportingFiles/Extensions/UIColors + Extension.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIColor {
+    static let universalBlackPrimary = UIColor(named: "UniversalBlackPrimary") ?? .black
+    static let universalBlue = UIColor(named: "UniversalBlue") ?? .blue
+    static let universalGray = UIColor(named: "UniversalGray") ?? .gray
+    static let universalGrayBackground = UIColor(named: "UniversalGrayBackground") ?? .gray
+    static let universalLightGray = UIColor(named: "UniversalLightGray") ?? .lightGray
+    static let universalLightOrange = UIColor(named: "UniversalLightOrange") ?? .orange.withAlphaComponent(0.2)
+    static let universalOrange = UIColor(named: "UniversalOrange") ?? .orange
+    static let universalWhite = UIColor(named: "UniversalWhite") ?? .white
+}
---
+++ BookIn/SupportingFiles/Extensions/UIFonts + Extension.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIFont {
+    static let largeTitleFont = UIFont.boldSystemFont(ofSize: 22)
+    static let mediumTitleFont = UIFont.boldSystemFont(ofSize: 18)
+    static let smallTitleFont = UIFont.boldSystemFont(ofSize: 16)
+    
+    static let extraLargeBodyFont = UIFont.boldSystemFont(ofSize: 30)
+    static let mediumBodyFont = UIFont.systemFont(ofSize: 16)
+    static let smallBodyFont = UIFont.systemFont(ofSize: 14)
+}
